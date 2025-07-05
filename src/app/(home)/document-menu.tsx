c40b455a971e48c46f25e04149cfe4c732d2d719
Manoj-Kumar-Munda <manojkrmunda828@gmail.com>
2025-07-05 11:39:31 +0530

    feat: add DocumentMenu component and integrate it into DocumentRow

---
+++ src/app/(home)/document-menu.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { ExternalLinkIcon, MoreVertical } from "lucide-react";
+import { Id } from "../../../convex/_generated/dataModel";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+interface DocumentMenuProps {
+  documentId: Id<"documents">;
+  title: string;
+  onNewTab: (id: Id<"documents">) => void;
+}
+
+const DocumentMenu = ({ documentId, title, onNewTab }: DocumentMenuProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon" className="rounded-full">
+          <MoreVertical className="size-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem onClick={() => onNewTab(documentId)}>
+          <ExternalLinkIcon className="size-4" />
+          Open in a new tab
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default DocumentMenu;
