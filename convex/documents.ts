1e5b311c30e18ec373c5cae8509d462e9bdb38f6
Manoj-Kumar-Munda <manojkrmunda828@gmail.com>
2025-07-05 12:05:48 +0530

    feat: add document removal functionality with confirmation dialog

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,5 +1,5 @@
 import { mutation, query } from "./_generated/server";
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
 import { paginationOptsValidator } from "convex/server";
 
 export const create = mutation({
@@ -26,5 +26,29 @@ export const get = query({
   args: { paginationOpts: paginationOptsValidator },
   handler: async (ctx, args) => {
     return await ctx.db.query("documents").paginate(args.paginationOpts);
+  },
+});
+
+export const removeById = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const user = await ctx.auth.getUserIdentity();
+
+    if (!user) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    const document = await ctx.db.get(args.id);
+
+    if (!document) {
+      throw new ConvexError("Document not found");
+    }
+
+    const isOwner = document.ownerId === user.subject;
+    if (!isOwner) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    return await ctx.db.delete(args.id);
   },
 });
---
+++ src/components/remove-dialog.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useMutation } from "convex/react";
+import { Id } from "../../convex/_generated/dataModel";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
+import { api } from "../../convex/_generated/api";
+import { useState } from "react";
+
+interface RemoveDialogProps {
+  documentId: Id<"documents">;
+  children: React.ReactNode;
+}
+
+export const RemoveDialog = ({ documentId, children }: RemoveDialogProps) => {
+  const remove = useMutation(api.documents.removeById);
+  const [isRemoving, setIsRemoving] = useState(false);
+
+  const handleDelete = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.stopPropagation();
+    setIsRemoving(true);
+    remove({ id: documentId }).finally(() => setIsRemoving(false));
+  };
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This cannot be undone. This will permanently delete your document.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={(e) => e.stopPropagation()}>
+            Cancel
+          </AlertDialogCancel>
+
+          <AlertDialogAction disabled={isRemoving} onClick={handleDelete}>
+            Delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
